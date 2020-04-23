70867e05a8302d90c7c04e5906afb1042efc898a
Rafael Oliveira <oliveirarafael@ua.pt>
2020-04-23 23:30:49 +0100

    halo ring model and starts added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var scene, camera, renderer, nebula_particles = [], composer
+var scene, camera, renderer, nebula_particles = [], composer, halo_ring
+
 
 function init() {
     scene = new THREE.Scene()
@@ -15,44 +16,103 @@ function init() {
 
     renderer = new THREE.WebGLRenderer()
     renderer.setSize(window.innerWidth, window.innerHeight)
-    scene.fog = new THREE.FogExp2('#000', 0.001)
-    renderer.setClearColor(scene.fog.color)
     document.body.appendChild(renderer.domElement)
+    
+    let loaderGLTF = new THREE.GLTFLoader();
+    loaderGLTF.load('img/sacred_ring_halo/scene.gltf', function(gltf){
+      halo_ring = gltf.scene.children[0];
+      halo_ring.position.set(0,450,-180);
+      halo_ring.rotation.set(-2.1,3.4,-1.2);
+      halo_ring.scale.set(0.2,0.2,0.2);
+      scene.add(gltf.scene);
+    });
+
+    let loader = new THREE.TextureLoader()
+    loader.load('img/stars.jpg', function (texture) {
+        scene.background = texture;  
+    })
 
     textureLoader('img/cloud_particle1.png', 20)
     textureLoader('img/cloud_particle2.png', 30)
     textureLoader('img/cloud_particle3.png', 20)
-
+    
+    
     hdr(renderer)
     render()
 }
 
+document.addEventListener('keypress', logKey);
+
+function logKey(e) {
+    console.log(e.code)
+
+    if(e.code == "KeyS"){
+        halo_ring.rotation.z -= 0.1;
+    }
+    if(e.code == "KeyW"){
+        halo_ring.rotation.z += 0.1;
+    }
+    if(e.code == "KeyA"){
+        halo_ring.rotation.x -= 0.1;
+    }
+    if(e.code == "KeyD"){
+        halo_ring.rotation.x += 0.1;
+    }
+    if(e.code == "KeyQ"){
+        halo_ring.rotation.y += 0.1;
+    }
+
+
+    if(e.code == "KeyE"){
+        halo_ring.rotation.y -= 0.1;
+    }
+    console.log(halo_ring.rotation)
+
+}
+  
+
+
 function render() {
     nebula_particles.forEach(p => {
-        p.rotation.z -= 0.0002
+        p.rotation.z -= 0.001
+
     })
+
+    try {
+        halo_ring.rotation.x -= 0.005;
+    } catch (error) {
+        
+    }
+
     composer.render(0.1)
     requestAnimationFrame(render)
 }
 
 init()
 
-window.addEventListener('resize', onWindowResize, false)
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth,window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+
+    camera.updateProjectionMatrix();
+}, false)
 
 function light() {
-    let directionalLight = new THREE.DirectionalLight('#d000ff')
+    let directionalLight = new THREE.DirectionalLight('#fff')
     directionalLight.position.set(0, 0, 1)
     scene.add(directionalLight)
 
-    let lightblue = new THREE.PointLight('#00ccff', 50, 450, 1.7)
-    lightblue.position.set(200, 300, 100)
-    scene.add(lightblue)
 
-    let pink = new THREE.PointLight('#ff00f2', 50, 450, 1.7)
+    let white = new THREE.PointLight('#a3e5ff', 50, 300, 6.5)
+    white.position.set( -120,  190,  -130)
+    
+    scene.add(white)
+
+    let pink = new THREE.PointLight('#ff00f2', 50, 450, 6.5)
     pink.position.set(200, 300, 100)
     scene.add(pink)
 
-    let blue = new THREE.PointLight('#2200ff', 50, 450, 1.7)
+    let blue = new THREE.PointLight('#2200ff', 50, 450, 6.5)
     blue.position.set(200, 300, 100)
     scene.add(blue)
 }
